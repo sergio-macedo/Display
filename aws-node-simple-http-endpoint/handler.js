5f3d6281669890c1482c7b2e75ac2c2043eb1058
Andre Paschoal <andre.paschoal@deliveryhero.com>
2021-07-26 14:47:17 +0200

    Add customer retrieval/creation on Stripe side, add stripe dependency.

--- aws-node-simple-http-endpoint/handler.js
+++ aws-node-simple-http-endpoint/handler.js
@@ -1,20 +1,34 @@
 'use strict';
 const stripe = require('stripe')('sk_test_51JEM9IE3nULJCHUVi9fkRqGi0OrCOt4hxKEhuqUQAiOPv8fwHdXIxFsZsXajmMJeDlJjM6I301gKnrjUzOqLhkVU00AXjGOkae');
 
-module.exports.endpoint = (event, context, callback) => {
-  
-  const email = event.body.email;
-  console.log(`customer's email: {$email}`);
-  
-  const customer = await stripe.customers.create(
-    {
-      email: email
-    }
-  );
+async function getCustomerId(email) {
+  const customers = await stripe.customers.list({
+    email: email,
+  });
 
+  if (customers.data.length > 0) {
+    const existingCustomer = customers.data[0];
+    console.log(`Existing customer ${JSON.stringify(existingCustomer)}`);
+    return existingCustomer.id;
+  }
 
+  const createdCustomer = await stripe.customers.create({ email: email });
+  console.log(`Created customer ${JSON.stringify(createdCustomer)}`);
+  return createdCustomer.id;
+}
 
+module.exports.endpoint = async (event, context, callback) => {
+  const body = JSON.parse(event.body);
 
+  // TODO add validation to all fields (email, value, desc)
+
+  const email = body.email;
+  console.log(`customer's email: ${email}`);
+  const customerId = await getCustomerId(email);
+  console.log(`Moving forward with customer id: ${customerId}`);
+
+  // TODO create invoice for customer. Use value and description. I think we should
+  //  add the cc email, probably Paulo's email.
 
 
   const response = {    
